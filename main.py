9d1fdeac518ddffc56b3f529fb07fa9ac2c14705
RajvanshMalhotra <136916006+RajvanshMalhotra@users.noreply.github.com>
2025-01-12 18:57:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ load_dotenv()
 import os
 from langchain_core.messages import BaseMessage,HumanMessage
 from langgraph.graph import END,MessageGraph
-# end contains the key for the langgrpahs default ending node, langgraph stops execution when this key is recieved
-# message graph: sequence  iof msgs, list of msgs,ouput will be one msg or a couple of msgs
 from chains import generation_chain, reflection_chain,process_reflection,process_tweet_feedback
 
 REFLECT="reflect"
 GENERATE="generate"
 
 def generate_node(state:Sequence[BaseMessage]):
-    # reecives the state which is a sequnece or list of msgs
     return generation_chain.invoke({"messages":state})
 
 
-def refelection_node(messages:Sequence[BaseMessage])-> List[BaseMessage]:   # the arrow mean what type of data the function will return
+def refelection_node(messages:Sequence[BaseMessage])-> List[BaseMessage]:   
     res=reflection_chain.invoke({"messages":messages})
     return [HumanMessage(content=res.content)]
 
@@ -32,20 +29,8 @@ def should_continue(state: List[BaseMessage]):
 builder.add_conditional_edges(GENERATE,should_continue)
 builder.add_edge(REFLECT,GENERATE)
 graph=builder.compile()
-# print(graph.get_graph())/
-# print(graph.get_graph().print_ascii())
-# graph.get_graph().print_ascii()
-
-
-
-
-
 
 if __name__=='__main__':
-    # aagar hum yeh file diorectly chalaye to tabhi niche wala code chalega, this is test code agar yeh file 
-    # file import hoti tho iska name== that other file ka name hota to niche wala code nhi chalta, its basically
-    # boiler plate code
-    # print("hello langgraph")
     print("Testing LangGraph...")
     inputs = HumanMessage(content="""Make this tweet better.
         - This new movie Pushpa 2 was not like the original one. It was trying to impersonate the general scenario set by the industry of extreme riches and showcasing PDA at every step.
@@ -55,8 +40,7 @@ if __name__=='__main__':
         print("Invoking graph with input...")
         results = graph.invoke(inputs)
         for result in results:
-            parsed_output = result.content  # Assuming result.content is a string
+            parsed_output = result.content  
             print(f"Output: {parsed_output}")
-        # print("Results:", results)
     except Exception as e:
         print(f"Error during graph invocation: {e}")
